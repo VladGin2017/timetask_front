50bb13544a5190958796bad4e5992f7c14fecf7d
vladislav <vlneudakhin@gmail.com>
2022-06-06 21:52:11 +0700

    Added mew header menu

--- timetask_front/src/App.js
+++ timetask_front/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import HomePage from './Pages/HomePage'
 import LoginPage from './Pages/LoginPage'
 import Register from './Pages/RegistrationPage'
@@ -9,65 +9,62 @@ import TodayTask from "./Pages/TodayTodos";
 import CompletedTodo from "./Pages/CompletedTodos";
 import NotFoundPage from "./Pages/NotFoundPage";
 
-export default function App() {
-        const classes = []
 
+export default function App(props) {
+    const navigate = useNavigate();
+    function clearStorage() {
+        localStorage.removeItem('token');
+        navigate('/login', {replace: true} );
+    }
     return (
-        <>  
-            <header className='header'>
-                {/* <div className='header__menu-container'>
-                    <Link to='/viewlist'>Список задач</Link>
-                </div> */}
-            </header>
-            <Routes>
-                <Route path="/" element={<TodoListPage></TodoListPage>}></Route>
-                {/* <Route path="/" element={<HomePage></HomePage>}></Route> */}
-                <Route path="/login" element={<LoginPage></LoginPage>}></Route>
-                <Route path="/register" element={<Register></Register>}></Route>
-                <Route path="/viewlist" element={<AllTasks></AllTasks>}></Route>
-                <Route path="/today" element={<TodayTask></TodayTask>}></Route>
-                <Route path="/completed" element={<CompletedTodo></CompletedTodo>}></Route>
-                <Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
-            </Routes>
-        </>
+        localStorage.getItem('token') ? (
+            <>
+                <header className='header'>
+                    <input type="checkbox" id="nav-toggle" hidden></input>
+                    <nav className="nav">
+                        <label for="nav-toggle" className="nav-toggle" onclick></label>
+                        <ul className="header__menu">
+                            <li><Link to="/" className="nav-menu__link">Главная</Link></li>
+                            <li><Link to="/viewlist" className="nav-menu__link">Список задач</Link></li>
+                        </ul>
+                            <button className="btn btn-logout" onClick={() => clearStorage()}>
+                                Выход
+                            </button>
+                    </nav>
+                </header>
+                <div>
+                    <Routes>
+                        <Route path="/viewlist" element={<TodoListPage></TodoListPage>}></Route>
+                        <Route path="/" element={<HomePage></HomePage>}></Route>
+                        <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+                        <Route path="/register" element={<Register></Register>}></Route>
+                        <Route path="/viewlist" element={<AllTasks></AllTasks>}></Route>
+                        <Route path="/today" element={<TodayTask></TodayTask>}></Route>
+                        <Route path="/completed" element={<CompletedTodo></CompletedTodo>}></Route>
+                        <Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
+                    </Routes>
+                </div>  
+            </>
+        ) : (
+            <>  
+                <header className='header'>
+                    <div className='header__menu-container'>
+                   
+                    </div>
+                </header>
+                <div>
+                    <Routes>
+                        <Route path="/viewlist" element={<TodoListPage></TodoListPage>}></Route>
+                        <Route path="/" element={<HomePage></HomePage>}></Route>
+                        <Route path="/login" element={<LoginPage></LoginPage>}></Route>                             
+                        <Route path="/register" element={<Register></Register>}></Route>
+                        <Route path="/viewlist" element={<AllTasks></AllTasks>}></Route>
+                        <Route path="/today" element={<TodayTask></TodayTask>}></Route>
+                        <Route path="/completed" element={<CompletedTodo></CompletedTodo>}></Route>
+                        <Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
+                    </Routes>
+                </div>
+            </>
+        )
     )
 }
-
-//  {
-//     if (localStorage.getItem('token')) {
-//         return (
-//         <div>
-//             <header className="nav-menu">
-//                 <div className="nav-menu__container">
-//                     <Link to='/' className="nav-menu__link">Главная</Link>
-//                     <Link to='/viewlist' className="nav-menu__link">Список задач</Link>
-//                 </div>
-//             </header>
-//             <Routes>
-//                 <Route path="/" element={<HomePage></HomePage>}></Route>
-//                 <Route path="/login" element={<LoginPage></LoginPage>}></Route>
-//                 <Route path="/register" element={<Register></Register>}></Route>
-//                 <Route path="/viewlist" element={<TodoListPage></TodoListPage>}></Route>
-//                 <Route path="/viewlist" element={<AllTasks></AllTasks>}></Route>
-//                 <Route path="/today" element={<TodayTask></TodayTask>}></Route>
-//                 <Route path="/completed" element={<CompletedTodo></CompletedTodo>}></Route>
-//             </Routes>
-//         </div>
-//         )
-//     } else {
-//         return (
-//             <div>
-//                 <header className="nav-menu">
-//                     <div className="nav-menu__container">
-//                         <Link to='/login' className="nav-menu__link">Вход</Link>
-//                         <Link to='/register' className="nav-menu__link">Регистрация</Link>
-//                     </div>
-//                 </header>
-//                 <Routes>
-//                     <Route path="/login" element={<LoginPage></LoginPage>}></Route>
-//                     <Route path="/register" element={<Register></Register>}></Route>
-//                 </Routes>
-//             </div>
-//             )
-//     }
-//     
